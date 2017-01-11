27b14e96e5f1b2730d3f77565e3120113fdb52c0
Zorca <vs@zorca.org>
2017-01-11 23:00:39 +0300

    Gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
+const gulp = require('gulp');
 const elixir = require('laravel-elixir');
+
+gulp.task("vendor", function() {
+  return gulp.src('./node_modules/font-awesome/fonts/*').pipe(gulp.dest('./public/fonts'));
+});
 
 elixir(function(mix) {
   mix.sass('./assets/scss/main.scss').webpack('./assets/js/main.js');
